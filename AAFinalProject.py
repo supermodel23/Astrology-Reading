f46dbd72ba99130bb6df1e6bf19a009f4bf2d692
supermodel23 <supermodel23@users.noreply.github.com>
2015-10-15 18:55:08 -0700

    Update AAFinalProject.py
    removed Flask code  and clarified 'Type Y or N' when asked if they want astrology reading.

--- AAFinalProject.py
+++ AAFinalProject.py
@@ -1,11 +1,3 @@
-from flask import Flask, render_template
-
-AAFinalProject = Flask(__name__)
-
-@AAFinalProject.route('/')
-def astrology():
-	return sign, compat_sign
-
 astro_dict = {"Aries":  "Adventurous and energetic, Pioneering and courageous, Enthusiastic and confident,\n Dynamic and quick-witted, Selfish and quick-tempered, Impulsive and impatient, Foolhardy and daredevil\n",
 "Taurus": "Patient and reliable, Warmhearted and loving, Persistent and determined, Placid and security loving,  Jealous and possessive, Resentful and inflexible, Self-indulgent and greedy\n"	,
 "Gemini": "Adaptable and versatile, Communicative and witty, Intellectual and eloquent, Youthful and lively,  Nervous and tense, Superficial and inconsistent, Cunning and inquisitive\n " ,
@@ -33,150 +25,130 @@ Compatibility_dict= {"Aries": "Aries is the passionate firebrand of the zodiac, 
 "Aquarius": "Whilst exciting and wild lovers, Aquarians aren't necessarily the most sentimental and romantic of the zodiac. Your Perfect Partner - Gemini and Libra. A Very Close Match - Aries and Sagittarius. Your Soulmate - Aquarius. Opposites Attract - Cancer and Virgo. You guys have your differences - Capricorn and Pisces. Going to Need Some Work - Taurus and Scorpio. Don't even bother - Leo" ,
 "Pisces": "Pisces are deeply romantic daydreamers. Incredibly sentimental, Pisces will shower their lovers with love. Your Perfect Partner - Cancer and Scorpio. A Very Close Match - Capricorn and Taurus. Your Soulmate - Pisces. Opposites Attract - Leo and Libra. You guys have your differences - Aries and Aquarius. Going to Need Some Work - Gemini and Sagittarius. Don't even bother - Virgo"}
 
-goodbye_msg = "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding!\n"
 
-def astro_reading():
+print "\n"+ "Hello! Welcome to your free astrology reading by April & Annette, by way of python.\n"
+name = raw_input('What is your name?\n')
 
-	print "\n"+ "Hello! Welcome to your free astrology reading by April & Annette, by way of python.\n"
-	name = raw_input('What is your name?\n')
+print "\n"+'Hello ' + name
+print "\n"+'Tell us your birthday to learn your astrological sign and personality traits\n'
+sign_reading = raw_input('Do you want to know what your astrological sign is? Type Yes or No.\n')
+sign_reading = sign_reading.lower()
 
-	print "\n"+'Hello ' + name
-	print "\n"+'Tell us your birthday to learn your astrological sign and personality traits\n'
-	sign_reading = raw_input('Do you want to know what your astrological sign is? Type Yes or No.\n')
-	sign_reading = sign_reading.lower()
-
-	if sign_reading=='yes' or sign_reading=='YES' or sign_reading=='y':
-	 	print "\n Awesome, let's get started! We need to know your birthday. \n" 
-	 	month = raw_input('Tell us your birth month. Make sure to type the full month i.e. December NOT Dec\n')
-	 	month = month.lower()
+if sign_reading=='yes' or sign_reading=='YES' or sign_reading=='y':
+ 	print "\n Awesome, let's get started! We need to know your birthday. \n" 
+ 	month = raw_input('Tell us your birth month. Make sure to type the full month i.e. December NOT Dec\n')
+ 	month = month.lower()
 
 
-		try:
-			if int(month) > 0:
-	 			print 'We need full month name, not the month number. Thank you.'
-	 			month = raw_input('Tell us your birth month. Make sure to type the full month i.e. December NOT Dec\n')
-	 			month = month.lower()
-		except:
-		 	pass
+	try:
+		if int(month) > 0:
+ 			print 'We need full month name, not the month number. Thank you.'
+ 			month = raw_input('Tell us your birth month. Make sure to type the full month i.e. December NOT Dec\n')
+ 			month = month.lower()
+	except:
+	 	pass
 
 
-	 	day = raw_input('Tell us the day of the month your were born \n')
-	 	day = int(day)
-	 	print "\n"
-	 	print month, day
-	 	
+ 	day = raw_input('Tell us the day of the month your were born \n')
+ 	day = int(day)
+ 	print "\n"
+ 	print month, day
+ 	
 
-		if month == "march" and day >= 21 or month == "april" and day <=20:
-			sign = "Aries"
-			print "You're an Aries!\n"
-			print astro_dict ["Aries"] 
-		if month == "april" and day >= 21 or month == "may" and day <=21:
-			sign = "Taurus"
-			print "You're an Taurus! \n"
-			print astro_dict ["Taurus"]
-		if month == "may" and day >= 21 or month == "june" and day <=21:
-			sign = "Gemini"
-			print "You're a Gemini!\n "
-			print astro_dict ["Gemini"]
-		if month == "june" and day >= 22 or month == "july" and day <=22:
-			sign = "Cancer"
-			print "You're a Cancer! \n"
-			print astro_dict ["Cancer"]
-		if month == "july" and day >= 23 or month == "august" and day <=21:
-			sign = "Leo"
-			print "You're a Leo\n! "
-			print astro_dict ["Leo"]
-		if month == "august" and day >= 22 or month == "september" and day <=23:
-			sign = "Virgo"
-			print "You're a Virgo!\n"
-			print astro_dict ["Virgo"]
-		if month == "september" and day >= 24 or month == "october" and day <=23:
-			sign = "Libra"
-			print "You're a Libra!\n"
-			print astro_dict ["Libra"]
-		if month == "october" and day >= 24 or month == "november" and day <=22:
-			sign = "Scorpio"
-			print "You're a Scorpio!\n"
-			print astro_dict ["Scorpio"]
-		if month == "november" and day >= 23 or month == "december" and day <=22:
-			sign = "Sagittarius"
-			print "You're a Sagittarius!\n"
-			print astro_dict ["Sagittarius"]
-		if month == "december" and day >= 23 or month == "january" and day <=20:
-			sign = "Capricorn"
-			print "You're a Capricorn!\n"
-			print astro_dict ["Capricorn"]
-		if month == "january" and day >= 21 or month == "february" and day <=19:
-			sign = "Aquarius"
-			print "You're an Aquarius!\n"
-			print astro_dict ["Aquarius"]
-		if month == "february" and day >= 20 or month == "march" and day <=20:
-			sign = "Pisces"
-			print "You're a Pisces!\n"
-			print astro_dict ["Pisces"]
-			
-
-			print "\n We aren't done yet."
-	else:
-		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding!"
-
+	if month == "march" and day >= 21 or month == "april" and day <=20:
+		sign = "Aries"
+		print "You're an Aries!\n"
+		print astro_dict ["Aries"] 
+	if month == "april" and day >= 21 or month == "may" and day <=21:
+		sign = "Taurus"
+		print "You're an Taurus! \n"
+		print astro_dict ["Taurus"]
+	if month == "may" and day >= 21 or month == "june" and day <=21:
+		sign = "Gemini"
+		print "You're a Gemini!\n "
+		print astro_dict ["Gemini"]
+	if month == "june" and day >= 22 or month == "july" and day <=22:
+		sign = "Cancer"
+		print "You're a Cancer! \n"
+		print astro_dict ["Cancer"]
+	if month == "july" and day >= 23 or month == "august" and day <=21:
+		sign = "Leo"
+		print "You're a Leo\n! "
+		print astro_dict ["Leo"]
+	if month == "august" and day >= 22 or month == "september" and day <=23:
+		sign = "Virgo"
+		print "You're a Virgo!\n"
+		print astro_dict ["Virgo"]
+	if month == "september" and day >= 24 or month == "october" and day <=23:
+		sign = "Libra"
+		print "You're a Libra!\n"
+		print astro_dict ["Libra"]
+	if month == "october" and day >= 24 or month == "november" and day <=22:
+		sign = "Scorpio"
+		print "You're a Scorpio!\n"
+		print astro_dict ["Scorpio"]
+	if month == "november" and day >= 23 or month == "december" and day <=22:
+		sign = "Sagittarius"
+		print "You're a Sagittarius!\n"
+		print astro_dict ["Sagittarius"]
+	if month == "december" and day >= 23 or month == "january" and day <=20:
+		sign = "Capricorn"
+		print "You're a Capricorn!\n"
+		print astro_dict ["Capricorn"]
+	if month == "january" and day >= 21 or month == "february" and day <=19:
+		sign = "Aquarius"
+		print "You're an Aquarius!\n"
+		print astro_dict ["Aquarius"]
+	if month == "february" and day >= 20 or month == "march" and day <=20:
+		sign = "Pisces"
+		print "You're a Pisces!\n"
+		print astro_dict ["Pisces"]
 		
 
-	Compatibility = raw_input('Do you want to know what sign you are or are not compatible with? Type Yes or No.\n')
-	if Compatibility =='Yes' or Compatibility=='yes' or Compatibility=='YES' or Compatibility=='Y':
-		if sign == "Aries":
-			compat_sign = Compatibility_dict ["Aries"]
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Taurus":
-			compat_sign = Compatibility_dict ["Taurus"]
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"  
-		if sign == "Gemini":
-			compat_sign =Compatibility_dict ["Gemini"]  
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Cancer":
-			compat_sign = Compatibility_dict ["Cancer"]  
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Leo":
-			compat_sign = Compatibility_dict ["Leo"] 
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Virgo":
-			compat_sign = Compatibility_dict ["Virgo"]  
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Libra":
-			compat_sign = Compatibility_dict ["Libra"]  
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Scorpio":
-			compat_sign = Compatibility_dict ["Scorpio"]  					
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Sagittarius":
-			compat_sign = Compatibility_dict ["Sagittarius"] 
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Capricorn":
-			compat_sign = Compatibility_dict ["Capricorn"]  
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Aquarius":
-			compat_sign = Compatibility_dict ["Aquarius"]  	 		
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-		if sign == "Pisces":
-			compat_sign = Compatibility_dict ["Pisces"] 
-			#print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
-			
-	
-		return  compat_sign, sign
-
-	else:
-		print goodbye_msg
+		print "\n We aren't done yet."
+else:
+	print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding!"
 	
 
-@AAFinalProject.route('/')
-def astro_web():
-	return sign, compat_sign
-
-print astro_reading()
-print goodbye_msg
-
-if __name__ == '__main__':
-	astro_web()
-	astro_reading()
-	AAFinalProject.run()
-
+Compatibility = raw_input('Do you want to know what sign you are or are not compatible with? Type Yes or No.\n')
+if Compatibility =='Yes' or Compatibility=='yes' or Compatibility=='YES' or Compatibility=='Y':
+	if sign == "Aries":
+		print Compatibility_dict ["Aries"]
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Taurus":
+		print Compatibility_dict ["Taurus"]
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"  
+	if sign == "Gemini":
+		print Compatibility_dict ["Gemini"]  
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Cancer":
+		print Compatibility_dict ["Cancer"]  
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Leo":
+		print Compatibility_dict ["Leo"] 
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Virgo":
+		print Compatibility_dict ["Virgo"]  
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Libra":
+		print Compatibility_dict ["Libra"]  
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Scorpio":
+		print Compatibility_dict ["Scorpio"]  					
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Sagittarius":
+		print Compatibility_dict ["Sagittarius"] 
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Capricorn":
+		print Compatibility_dict ["Capricorn"]  
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Aquarius":
+		print Compatibility_dict ["Aquarius"]  	 		
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	if sign == "Pisces":
+		print Compatibility_dict ["Pisces"] 
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding! And good luck looking for your best mate.\n"
+	pass	
+else:
+		print "\nThank you for your time. We hope you had fun! Go to www.hackbrightacademy.com to learn more about coding!"
+	
